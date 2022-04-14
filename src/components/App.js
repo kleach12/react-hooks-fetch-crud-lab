91a1abe3a5b3aeec23d72f302197e230074903bf
Kale Leach <kleach1212@gmail.com>
2022-04-13 17:37:47 -0700

    commit tree cb8bb5646b319167a8ab3cddf1401a8a3311cc50

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+  // console.log(questions)
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data)
+        setQuestions(data)
+    })
+  },[])
+
+  function handleDeletedQuestion(deletedQuestion){
+    const deleteQuest = questions.filter((question) => question.id !== deletedQuestion.id )
+    setQuestions(deleteQuest)
+    console.log(deletedQuestion.id)
+  }
+
+  function handleAddQuestion (quest){
+    setQuestions([...questions, quest])
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onAddQuestion = {handleAddQuestion}/> : <QuestionList question ={questions} onDelete = {handleDeletedQuestion}/>}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,18 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({question, onDelete}) {
+
+
+  const soloQuestion = question.map((quest) => (
+    <QuestionItem key = {quest.id} question = {quest} deleteQuestion = {onDelete} />
+  ))
+  //console.log(soloQuestion)
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{soloQuestion}</ul>
     </section>
   );
 }
